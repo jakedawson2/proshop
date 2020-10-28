b9c77a610d37b42171d76b9b5294f74d18b96442
Jake Dawson <jake@upbound.com.au>
2020-10-28 21:12:02 +1100

    first 2 sections of the course

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const Product = ({ product }) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Product
