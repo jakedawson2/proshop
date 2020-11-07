0b44a7a25aaa1b64db55788dd3a04c356f01d44a
Jake Dawson <jake@upbound.com.au>
2020-11-08 07:34:35 +1100

    dummy chage to test github

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -18,7 +18,7 @@ const HomeScreen = () => {
 
   return (
     <>
-      <h1>Lastest Products</h1>
+      <h1>Not the Lastest Products</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
