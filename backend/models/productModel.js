f0702726f53cab772bef59555e8bea0210032b4a
upbound-web <jake@upbound.com.au>
2020-11-11 19:58:38 +1100

    data seeder

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -13,7 +13,7 @@ const productSchema = mongoose.Schema(
     user: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
-          ref = 'User'
+          ref: 'User'
       },
     name: {
       type: String,
