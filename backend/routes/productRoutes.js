624904d3208c2fa9a55902bbd0f45b7b2beb7319
upbound-web <jake@upbound.com.au>
2020-11-14 19:46:22 +1100

    setup redux for product list. Adder loader image and error message

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,22 +6,28 @@ import Product from '../models/productModel.js'
 // @desc Fetch all products
 // @route GET /api/products
 // @access public
-router.get('/', asyncHandler( async (req, res) => {
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
     const products = await Product.find({})
     res.json(products)
-  }))
+  })
+)
 
- // @desc Fetch a single product
+// @desc Fetch a single product
 // @route GET /api/products/:id
-// @access public 
-  router.get('/:id', asyncHandler (async (req, res) => {
+// @access public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
-        if(product) {
-            res.json(product)
-        } else {
-            res.status(404)
-            throw new Error('Product not found')
-        }
-  }))
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+)
 
-  export default router
+export default router
