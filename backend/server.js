da58be253960a0bbdb4c3989aeda88065c333b8d
upbound-web <jake@upbound.com.au>
2020-11-14 16:28:52 +1100

    reducers

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { notFound, errorHandler} from './middleware/errorMiddleware.js'
 import colors from 'colors'
 
 import productRoutes from './routes/productRoutes.js'
@@ -17,20 +18,11 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`)
-  res.status(404)
-  next(error)
-})
+app.use(notFound)
 
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-  res.status(statusCode)
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  })
-})
+app.use(errorHandler)
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(
---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,6 @@
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../constants/productConstants'
+
+
+export const listProducts = () => {
+    
+}
