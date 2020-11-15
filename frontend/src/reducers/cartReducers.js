c090ca548d98b022a0b173e55b3389c9d720087c
upbound-web <jake@upbound.com.au>
2020-11-15 18:09:13 +1100

    cart reducer and actions for remove

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,11 +1,11 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload
 
-      const existItem = state.cartItems.find((x) => x.products === item.product)
+      const existItem = state.cartItems.find((x) => x.product === item.product)
 
       if (existItem) {
         return {
@@ -19,6 +19,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           ...state,
           cartItems: [...state.cartItems, item]
         }
+      }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload)
       }
     default:
       return state
